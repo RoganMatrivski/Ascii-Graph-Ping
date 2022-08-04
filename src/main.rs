d0f47e6a12a4bf02ca22172e81c208e1c9d38698
Robin Mauritz <robinmauritzrm@gmail.com>
2022-08-04 12:38:14 +0700

    feat: Pinger now works on Windows

--- src/main.rs
+++ src/main.rs
@@ -147,6 +147,43 @@ async fn ping_processor(mut rx: Receiver<r#struct::PingResult>, tx: Sender<Vec<u
     }
 }
 
+#[cfg(target_os = "windows")]
+async fn pinger(
+    ip: std::net::IpAddr,
+    ip_id: usize,
+    tx: Sender<r#struct::PingResult>,
+    start_time: Instant,
+) {
+    let pinger = winping::AsyncPinger::new();
+    let delay_time = Duration::from_millis(DELAY_TIME_MILLIS);
+    let mut next_wait = start_time + delay_time;
+
+    let mut seq: usize = 0;
+
+    loop {
+        let buf = winping::Buffer::new();
+        let probably_rtt = pinger.send(ip, buf).await.result;
+        tx.send(r#struct::PingResult {
+            ip_id,
+            seq,
+            rtt: match probably_rtt {
+                Ok(rtt) => Some(Duration::from_millis(rtt.into())),
+                Err(err) => {
+                    println!("{:?}", err);
+                    continue;
+                }
+            },
+        })
+        .await
+        .unwrap();
+
+        tokio::time::sleep_until(next_wait).await;
+        next_wait += delay_time;
+        seq += 1;
+    }
+}
+
+#[cfg(target_os = "unix")]
 async fn pinger(
     ip: std::net::IpAddr,
     ip_id: usize,
